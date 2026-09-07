b91dc241a0ac0e0a3bb1a5aea42862df52905bbc
napun87 <accmaruthunhat@gmail.com>
2026-09-07 14:35:33 +0700

    Update lemonhubV2.lua

--- lemonhubV2.lua
+++ lemonhubV2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=h4x"))()
+aloadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/main/stealaneggv2"))()
